a0d1132dd2832ce0329806bf6b51cb55da5fa380
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2017-12-30 16:32:53 +0000

    Modularize temperature reading

--- main.js
+++ main.js
@@ -1,66 +1,17 @@
-const fs = require('fs');
 const request = require('request');
+const temperature = require('./temperature');
+const get_temperature = temperature.init(1000, 5);
 
 const url = 'http://kolodziejski.me/mirror/data/data.php';
 
-function median (arr) {
-  if (arr.length === 0) {
-    return null;
-  }
-  arr.sort();
-  if (arr.length % 2 === 0) {
-    return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2;
-  } else {
-    return arr[(arr.length - 1) / 2];
-  }
-}
-
-// https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing?view=all
-function doRead (deviceId) {
-  let filename = '/sys/bus/w1/devices/' + deviceId + '/w1_slave';
-  let result = fs.readFileSync(filename).toString();
-  if (/crc=.*YES/.test(result)) {
-    return (+result.match(/t=(-?\d+)/)[1]) / 1000;
-  } else {
-    throw 'No correct temperature found: ' + filename + '(' + result + ')';
-  }
-}
-
-var read = (function () {
-  var temperatures = [];
-
-  function loop() {
-    try {
-      temperatures.push(doRead('28-0216252dbfee'));
-    } catch (e) {
-      console.error(e);
-    }
-    setTimeout(loop, 2000);
-  }
-  loop();
-
-  function getResult () {
-    var data = {
-      temperature: median(temperatures),
-      timestamp: Date.now()
-    };
-
-    temperatures = [];
-
-    if (data.temperature != null) {
-      return data;
-    } else {
-      return null;
-    }
-  }
-
-  return getResult;
-})();
-
 function readAndSend () {
-  var data = read();
-  if (data != null) {
-console.log(data);
+  var temperature = get_temperature();
+  if (temperature != null) {
+    let data = {
+      temperature: temperature,
+      timestamp: Data.now()
+    };
+    console.log('temperature: ' + data);
     request.post({url, form: {data: JSON.stringify(data)}}, function (error, httpResponse, body) {
       // log?
     });
---
+++ temperature.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+function median (arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+  arr.sort();
+  if (arr.length % 2 === 0) {
+    return (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2;
+  } else {
+    return arr[(arr.length - 1) / 2];
+  }
+}
+
+// https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing?view=all
+function doRead (deviceId) {
+  let filename = '/sys/bus/w1/devices/' + deviceId + '/w1_slave';
+  let result = fs.readFileSync(filename).toString();
+  if (/crc=.*YES/.test(result)) {
+    return (+result.match(/t=(-?\d+)/)[1]) / 1000;
+  } else {
+    throw 'No correct temperature found: ' + filename + '(' + result + ')';
+  }
+}
+
+var temperatures = [];
+function loop(samples) {
+  try {
+    temperatures.push(doRead('28-0216252dbfee'));
+    temperatures = temperatures.slice(-samples);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function getResult () {
+  return median(temperatures);
+}
+
+module.exports = {
+  init : function (span, samples) {
+    setInterval(() => loop(samples), span);
+    loop(samples);
+    return getResult;
+  }
+}
