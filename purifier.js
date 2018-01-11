f686ddb4546ab30a04a9e4566ffd0763d7c7e99c
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2018-01-11 12:52:11 +0000

    Don't poll purifier

--- purifier.js
+++ purifier.js
@@ -3,29 +3,31 @@ const miio = require('miio');
 // TODO: Make sure this data is not too old
 module.exports = function (address, span) {
   let device = miio.device({address: address, retries: 5});
+  let data = {};
   device.then(device => {
-    device.setBuzzer(false)
-    
+    device.setBuzzer(false);
+
     let properties = new Set(['aqi', 'temperature', 'humidity', 'mode']);
-  
-    let data = {};
+
     for (let p of properties) {
       data[p] = device[p];
     }
-  
+
     device.on('propertyChanged', e => {
       if (properties.has(e.property)) {
         data[e.property] = e.value;
       }
     });
+  }).catch(console.error);
 
-    return {
-      getData: function () {
-        return data;
-      },
-      setMode: function (mode) {
+  return {
+    getData: function () {
+      return data;
+    },
+    setMode: function (mode) {
+      return device.then(device => {
         return device.setMode(mode);
-      },
-    };
-  });
+      });
+    }
+  };
 };
