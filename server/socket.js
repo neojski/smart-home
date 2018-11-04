c748917c66d704d619fa1293bb43c5c984a0ab4e
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2018-11-04 20:25:22 +0000

    Improve reconnection logic

--- server/socket.js
+++ server/socket.js
@@ -2,47 +2,54 @@ const TuyAPI = require('tuyapi');
 const timestamp = require('./timestamp');
 const EventEmitter = require('events');
 
-
 module.exports = function ({id, key}) {
   const emitter = new EventEmitter();
   const device = new TuyAPI({id, key, persistentConnection: true});
 
-  let result;
-  let status;
+  let result = {};
 
-  let theOneDevice;
-  async function setup () {
-    try {
-      if (theOneDevice != null) {
-        theOneDevice.disconnect();
-      }
-
-      await device.resolveId();
-      console.log('resolved socket');
-
-      device.on('connected',() => {theOneDevice = device;});
-      device.on('disconnected',() => { setup(); });
-      device.on('error',(err) => { setup(); });
-
-      device.on('data', data => {
-        console.log('socket data', data);
-        if (data.dps['1'] != null) {
-          status = data.dps['1'];
-        }
-        result = {
-          status: status,
-          timestamp: timestamp()
-        };
-        emitter.emit('data', result);
-      });
-      return device.connect();
-    } catch(e) {
-      console.error('Socket issue, restarting socket connection. Retrying in 5s.', e);
-      setTimeout(setup, 5000);
-    }
+  function resolveId (done) {
+    // This fails when one tries to discover multiple devices at the same time
+    device.resolveId().then(done).catch((error) => {
+      console.error('Could not resolveId. Trying again in 5s', error);
+      setTimeout(() => { resolveId(done); }, 5000);
+    });
   }
 
-  setup();
+  resolveId(() => {
+    let resetting = false;
+    function reset (reason) {
+      // TODO: I'm not sure this makes any sense with persistent connection. It all seems pretty unstable
+      if (resetting) {
+        return;
+      }
+      device.disconnect();
+      console.error('Restarting socket connection. Retrying in 5s.', reason);
+      resetting = true;
+      setTimeout(() => {
+        resetting = false;
+        device.connect ();
+      }, 5000);
+    }
+
+    device.on('disconnected', () => { reset('disconnected'); });
+    device.on('error', (e) => { reset(e); });
+
+    device.on('data', data => {
+      console.log('socket data', data);
+      if (data.dps['1'] != null) {
+        status = data.dps['1'];
+      }
+      result = {
+        status: status,
+        timestamp: timestamp()
+      };
+      emitter.emit('data', result);
+    });
+
+    device.connect();
+  });
+
 
   emitter.getData = function () {
     return result;
