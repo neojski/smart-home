295883c6663f56ac37c939fc02f6b726a1482c81
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2020-02-02 18:56:35 +0000

    Add visible vs hidden clock test

--- ui/__tests__/clock.test.tsx
+++ ui/__tests__/clock.test.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import renderer, { act } from "react-test-renderer";
 import { Clock } from "../Clock";
-import { advanceTo } from "jest-date-mock";
+import { advanceTo, advanceBy } from "jest-date-mock";
 
+jest.useFakeTimers();
 it("renders correctly", async () => {
   advanceTo(new Date(2020, 1, 1, 12, 13, 14));
 
@@ -41,5 +42,45 @@ it("renders correctly", async () => {
       </span>
     </div>
   `);
+
+  act(() => {
+    advanceBy(1000);
+    jest.advanceTimersByTime(1000);
+  });
+  expect(testRenderer.toJSON()).toMatchInlineSnapshot(`
+<div
+  style={
+    Object {
+      "fontSize": "300px",
+      "fontWeight": 300,
+      "textAlign": "center",
+    }
+  }
+>
+  12
+  <span
+    style={
+      Object {
+        "visibility": "visible",
+      }
+    }
+  >
+    :
+  </span>
+  13
+  <span
+    style={
+      Object {
+        "display": "inline-block",
+        "fontSize": "30%",
+        "transform": "translate(0, -30px) rotate(-90deg)",
+      }
+    }
+  >
+    15
+  </span>
+</div>
+`);
+
   testRenderer.unmount();
 });
