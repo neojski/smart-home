c2dd110c02b84dcbf7ee7b5b88d5de9fe03a4deb
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2023-06-27 19:36:30 +0100

    Remove kiosk flag as it makes interacting with chrome difficult

--- start_chromium.sh
+++ start_chromium.sh
@@ -1,3 +1,3 @@
 # https://superuser.com/questions/237608/how-to-hide-chrome-warning-after-crash
 export DISPLAY=:0
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences && /usr/bin/chromium-browser --kiosk 127.0.0.1:3000
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences && /usr/bin/chromium-browser 127.0.0.1:3000
