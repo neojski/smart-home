c43b0680052a10d0adae031633eba03a764d9fd2
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2023-07-01 00:50:30 +0100

    WIP

--- ui/homeAssistant.tsx
+++ ui/homeAssistant.tsx
@@ -3,60 +3,114 @@
 import { Data } from "./Data";
 
 export default class {
-  interval;
+  data: Data;
   update: { (data: Data): void };
+  socket: WebSocket;
+  next: number;
+
+  send(
+    query: any,
+    { suppressId }: { suppressId: boolean } = { suppressId: false }
+  ) {
+    if (!suppressId) {
+      const id = this.next++;
+      query.id = id;
+    }
+    this.socket.send(JSON.stringify(query));
+  }
 
   constructor(update: { (data: Data): void }) {
     // CR: actually read data from websockets
     this.update = update;
-    this.interval = setInterval(this.refresh.bind(this), 5000);
-    this.refresh();
-  }
 
-  refresh() {
-    function noise(n: number) {
-      return n + Math.floor(100 * Math.random());
-    }
-    this.update({
-      aqi: 999,
-      power: noise(500),
-      upTemperature: 999,
-      downTemperature: 999,
-      weather: { main: { temp: 999 }, weather: [{ icon: "01d" }] },
+    // CR: not sure about this error handling
+    this.data = {
+      aqi: undefined,
+      power: undefined,
+      upTemperature: undefined,
+      downTemperature: undefined,
+      weather: "undefined",
+    };
+    this.next = 0;
+
+    const HA_WS_API_URL = "ws://192.168.1.232:8123/api/websocket";
+    const HA_ACCESS_TOKEN =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiM2IxYzI1ZmViOGI0YzE5OTM2ZDYyZWQwM2E4OTNmYyIsImlhdCI6MTY4ODE1NzA5NywiZXhwIjoyMDAzNTE3MDk3fQ.O6sL-X2bB7ztsEitEhkGtzW6z-O_f75JlVee8dUDq24";
+    // Connect to Home Assistant WebSocket API
+    this.socket = new WebSocket(HA_WS_API_URL);
+    this.socket.addEventListener("open", () => {
+      console.log("Connected to Home Assistant WebSocket API");
+
+      // Authenticate with Home Assistant
+      this.send(
+        { type: "auth", access_token: HA_ACCESS_TOKEN },
+        // auth forbids ids
+        { suppressId: true }
+      );
+
+      // Configuration
+      this.socket.addEventListener("message", (event: any) => {
+        const message = JSON.parse(event.data);
+
+        if (message.type === "auth_ok") {
+          console.log("Authentication successful");
+
+          // Also subscribe to events
+          this.send({ type: "subscribe_events" });
+
+          // This should cause message.type "result". We do it to get initial snapshot of events
+          this.send({ type: "get_states" });
+        } else if (message.type === "message") {
+          // CR this is untested
+          this.refresh(message);
+        } else if (message.type === "result") {
+          if (message.result) {
+            this.refresh(message.result);
+          } else {
+            console.warn("message.result is falsy", message);
+          }
+        } else {
+          console.warn("ignored message kind", message);
+        }
+      });
+      this.socket.addEventListener("close", () => {
+        console.log("Disconnected from Home Assistant WebSocket API");
+      });
+      this.socket.addEventListener("error", (error) => {
+        console.error("WebSocket error:", error);
+      });
     });
   }
 
+  // CR-someday: sad types
+  refresh(results: any[]) {
+    for (const result of results) {
+      if (result.entity_id === "ensor.openweathermap_temperature") {
+        this.data.weather = {
+          main: {
+            temp: Number(result.state),
+          },
+          weather: [
+            {
+              icon: "01d",
+              // CR fix
+            },
+          ],
+        };
+      }
+    }
+
+    function noise(n: number) {
+      return n + Math.floor(100 * Math.random());
+    }
+
+    // CR other bits
+    this.data.power = noise(500);
+
+    this.update(this.data);
+  }
+
   destroy() {
-    clearInterval(this.interval);
+    this.socket.close();
   }
 }
-
-// Configuration
-const HA_WS_API_URL = "ws://192.168.1.232:8123/api/websocket";
-const HA_ACCESS_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJiM2IxYzI1ZmViOGI0YzE5OTM2ZDYyZWQwM2E4OTNmYyIsImlhdCI6MTY4ODE1NzA5NywiZXhwIjoyMDAzNTE3MDk3fQ.O6sL-X2bB7ztsEitEhkGtzW6z-O_f75JlVee8dUDq24";
-// Connect to Home Assistant WebSocket API
-const socket = new WebSocket(HA_WS_API_URL);
-socket.addEventListener("open", () => {
-  console.log("Connected to Home Assistant WebSocket API");
-
-  // Authenticate with Home Assistant
-  socket.send(JSON.stringify({ type: "auth", access_token: HA_ACCESS_TOKEN }));
-});
-socket.addEventListener("message", (event: any) => {
-  const message = JSON.parse(event.data);
-
-  if (message.type === "auth_ok") {
-    console.log("Authentication successful");
-
-    socket.send(JSON.stringify({ id: 1, type: "subscribe_events" }));
-  }
-
-  console.log(event);
-});
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Home Assistant WebSocket API");
-});
-socket.addEventListener("error", (error) => {
-  console.error("WebSocket error:", error);
-});
