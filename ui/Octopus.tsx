80fe952d7713413ccb775c190a13e303d5435193
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2023-07-01 13:48:11 +0100

    WIP

--- ui/Octopus.tsx
+++ ui/Octopus.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { errorSpan } from "./errorSpan";
 
-// TODO: check freshness of this data?
 export function Octopus({ power }: { power: number | undefined }) {
   const content = power === undefined ? errorSpan() : "" + Math.round(power);
   return (
