7a815cb3c2d477d0c7b4eb894ec07cf97f61a6d8
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2024-03-16 21:51:44 +0000

    systemd service update

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Server and UI for smart mirror that controls my home
 
 Most data is fetched from my local instance of home assistant
+
+# install systemd
+1. Copy service to /lib/systemd/system/kiosk.service
+2. sudo systemctl daemon-reload
+3. sudo systemctl start kiosk
+4. sudo systemctl enable kiosk
--- start_chromium.sh
+++ start_chromium.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # https://superuser.com/questions/237608/how-to-hide-chrome-warning-after-crash
 export DISPLAY=:0
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences && /usr/bin/chromium-browser --start-fullscreen ~/smart-home/ui/index.html
+# --no-daemon seems important for systemd not to start multiple instances
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences && /usr/bin/chromium-browser --kiosk --no-daemon ~/smart-home/ui/index.html
