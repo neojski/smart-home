b955e3050c2378794be5eacd2cdcc17684e5ea2f
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2024-03-16 22:14:46 +0000

    Change from kiosk to start-fullscreen as that makes it easier to actually stop fullscreen

--- start_chromium.sh
+++ start_chromium.sh
@@ -2,4 +2,4 @@
 # https://superuser.com/questions/237608/how-to-hide-chrome-warning-after-crash
 export DISPLAY=:0
 # --no-daemon seems important for systemd not to start multiple instances
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences && /usr/bin/chromium-browser --kiosk --no-daemon ~/smart-home/ui/index.html
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences && /usr/bin/chromium-browser --start-fullscreen --no-daemon ~/smart-home/ui/index.html
