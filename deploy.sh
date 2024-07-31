1cf4cf53bd3c27f411216c08f7259de9b0b4da4b
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2024-07-31 21:45:33 +0100

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,6 @@ set -e -u -o pipefail
 
 echo 'Deploying to pi'
 rsync -avz --exclude .git --exclude node_modules . pi:~/smart-home
+
+echo 'Restarting smart-home'
+ssh pi systemctl --user restart smart-home
