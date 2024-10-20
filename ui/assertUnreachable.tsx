64e9db253cce137bc2bbc354c981063c8a0c28a2
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2024-10-20 20:17:25 +0100

    Remove more unused code

--- ui/assertUnreachable.tsx
+++
@@ -1,3 +0,0 @@
-export function assertUnreachable(_value: never): never {
-  throw new Error("Statement should be unreachable");
-}
--- ui/heatingStyle.tsx
+++
@@ -1,7 +0,0 @@
-export function heatingStyle(isOn: boolean) {
-  if (isOn) {
-    return { borderRadius: "30px", background: "#fff", color: "#000" };
-  } else {
-    return {};
-  }
-}
