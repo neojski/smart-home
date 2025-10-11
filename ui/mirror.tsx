2f71d9e730770c73f18faa120c82e177b30c5ed9
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2025-10-11 22:13:40 +0100

    Use svg icon

--- ui/mirror.tsx
+++ ui/mirror.tsx
@@ -26,7 +26,27 @@ export function Main() {
       <div
         style={{ textAlign: "right", fontSize: "50px", marginRight: "80px" }}
       >
-        🚗 {data.teslaBattery}%
+        {
+          // https://tabler.io/icons/icon/car
+        }
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100"
+          height="100"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.4"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          style={{ verticalAlign: "middle" }}
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+          <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+          <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5" />
+        </svg>
+        <span style={{ verticalAlign: "middle" }}>{data.teslaBattery}%</span>
       </div>
       <Aqi aqi={data.aqi} />
       <Clock />
