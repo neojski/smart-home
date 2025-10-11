661761064da7d5cc8b14ce58786781b3356cc202
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2025-10-11 22:16:20 +0100

    Thicker font

--- ui/mirror.tsx
+++ ui/mirror.tsx
@@ -46,7 +46,9 @@ export function Main() {
           <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
           <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5" />
         </svg>
-        <span style={{ verticalAlign: "middle" }}>{data.teslaBattery}%</span>
+        <span style={{ verticalAlign: "middle", fontWeight: 600 }}>
+          {data.teslaBattery}%
+        </span>
       </div>
       <Aqi aqi={data.aqi} />
       <Clock />
